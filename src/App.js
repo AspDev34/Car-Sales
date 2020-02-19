8d6c8f83bc20c1b5523a59b2f7f72788fd192418
Ike Otu <ikeotu@msn.com>
2020-02-19 00:19:29 -0800

    all components functional except appReducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 
 import Header from './components/Header';
-import AddedFeatures from './components/AddedFeatures';
+// import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
 const App = () => {
   
 
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
-
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
+  
 
   return (
     <div className="boxes">
       <div className="box">
         <Header />
-        <AddedFeatures />
+        {/* <AddedFeatures /> */}
       </div>
       <div className="box">
         <AdditionalFeatures />
--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
+import { connect } from 'react-redux';
+import { buyItem } from '../actions/removebuyActions';
 
 const AdditionalFeatures = props => {
   return (
@@ -8,7 +10,7 @@ const AdditionalFeatures = props => {
       {props.additionalFeatures.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature key={item.id} feature={item} buyItem={props.buyItem}/>
           ))}
         </ol>
       ) : (
@@ -18,4 +20,18 @@ const AdditionalFeatures = props => {
   );
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = (initialState) => {
+  return {
+    additionalFeatures: initialState.additionalFeatures
+  };
+};
+
+export default connect(mapStateToProps, { buyItem })(AdditionalFeatures); // calling a function twice in this manner is known as 'function currying'
+
+//connect is a function that gets called twice!
+//The first call: takes two arguments-- #1 is a function conventionally called mapStateToProps().
+//#2 is an object of action creaters. An action creater is a function that returns an action object. 
+//Under the hood, redux will take that action object that was returned, and dispatch it to the reducer. 
+
+
+//The second call: takes the component you want to connect as an argument.
